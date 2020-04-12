1fcec7ad56fff6128b91a79c06d6c97f6094d4a4
yoonah1996 <yoonah1996@gmail.com>
2020-04-12 20:45:15 +0900

    04/12-yoonah-Home.js-function change

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,49 +1,43 @@
 import { withRouter } from 'react-router-dom';
-import React, { Component } from 'react'
+import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap';
 
-class Home extends Component {
-    constructor(props) {
-        super(props)
 
-        this.state = {
-        }
-    }
-
-    render() {
-        if (this.props.isLogin) {
-            return (
-                <Container>
-                    <Row>
-                        <Col>
-                            <button onClick={() => {
-                                this.props.history.push('/Mypage')
-                            }}>mypage</button> </Col>
-                        <Col>
-                            <div>
+function Home(props) {
+    if (props.isLogin) {
+        return (
+            <Container>
+                <Row>
+                    <Col>
+                        <button onClick={() => {
+                            props.history.push('/Mypage')
+                        }}>mypage</button> </Col>
+                    <Col>
+                        <div>
+                            <p />
+                            운동을 선택해 주세요!
                                 <p />
-                                운동을 선택해 주세요!
-                                <p />
-                                <button className = 'exercise' onClick={() => {
-                                    this.props.history.push('/Start')
-                                    this.props.selectExercise(document.querySelector('.exercise').innerHTML)
-                                }}>squat</button>
-                                <button onClick={() => { }}>준비중</button>
-                                <button onClick={() => { }}>준비중</button>
-                            </div>
-                        </Col>
-                        <Col> </Col>
-                    </Row>
-                </Container>
-            )
-        } else {
-            return (
-                <div>
-                    {this.props.history.push('/Login')}
-                </div>
-            )
-        }
+                            <button className='exercise' onClick={() => {
+                                props.history.push('/Start')
+                                props.selectExercise(document.querySelector('.exercise').innerHTML)
+                            }}>squat</button>
+                            <button onClick={() => { }}>준비중</button>
+                            <button onClick={() => { }}>준비중</button>
+                        </div>
+                    </Col>
+                    <Col> </Col>
+                </Row>
+            </Container>
+        )
+    } else {
+        return (
+            <div>
+                {props.history.push('/Login')}
+            </div>
+        )
     }
 }
 
-export default withRouter(Home)
+
+
+    export default withRouter(Home)
