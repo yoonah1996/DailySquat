33225d81eb72103a47a5ff70585e160ba3b6a53c
river <river9028@gmail.com>
2020-04-16 22:10:41 +0900

    04/16-river-Home, Result 페이지 디자인 수정

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,10 +1,13 @@
 import { withRouter } from 'react-router-dom';
 import React, { Component } from 'react'
-import { Container, Row, Col, Button } from 'react-bootstrap';
+// import { Container, Row, Col, Button } from 'react-bootstrap';
 
 import styled from 'styled-components';
 
 import image from './image/meghan-holmes-wy_L8W0zcpI-unsplash.jpg';
+
+
+import './Home.css'
 
 const Background = styled.div`
     position: absolute;
@@ -27,7 +30,6 @@ const Background = styled.div`
     -moz-osx-font-smoothing: grayscale;  
 `;
 
-
 class Home extends Component {
   constructor(props) {
     super(props)
@@ -35,37 +37,84 @@ class Home extends Component {
 
     }
   }
+  
 
   render() {
     console.log('this.props :', this.props);
     if (this.props.isLogin) {
       return (
+
+        // <Container>
+        //   <Row>
+        //     <Col>
+        //       <Button variant="secondary" onClick={() => {
+        //         this.props.getUserInfo();
+        //         // this.props.getTotalCount();
+        //         this.props.history.push('/Mypage');
+        //       }}>mypage</Button> </Col>
+        //     <Col>
+        //       <div>
+        //         <p />
+        //         운동을 선택해 주세요!
+        //         <p />
+        //         <Button variant="light" className='exercise' onClick={() => {
+          //           this.props.history.push('/Start')
+        //           this.props.selectExercise(document.querySelector('.exercise').innerHTML)
+        //         }}>squat</Button>
+        //         <Button variant="light" onClick={() => { }}>준비중</Button>
+        //         <Button variant="light" onClick={() => { }}>준비중</Button>
+        //       </div>
+        //     </Col>
+        //     <Col> </Col>
+        //   </Row>
+        // </Container>
+        
         <Background>
 
-          <Container>
-            <Row>
-              <Col>
-                <Button variant="secondary" onClick={() => {
-                  this.props.getUserInfo();
-                  // this.props.getTotalCount();
-                  this.props.history.push('/Mypage');
-                }}>mypage</Button> </Col>
-              <Col>
-                <div>
-                  <p />
-                운동을 선택해 주세요!
-                <p />
-                  <Button variant="light" className='exercise' onClick={() => {
-                    this.props.history.push('/Start')
-                    this.props.selectExercise(document.querySelector('.exercise').innerHTML)
-                  }}>squat</Button>
-                  <Button variant="light" onClick={() => { }}>준비중</Button>
-                  <Button variant="light" onClick={() => { }}>준비중</Button>
+          <div className="container-box">
+            <p href="" alt="" onClick={() => {
+                  this.props.history.push('/Start')
+                  this.props.selectExercise(document.querySelector('.exercise').innerHTML)
+                }}>
+              <div className="card-item">
+                <div className="card-inner">
+                  <i className="fas fa-medal">SQUAT</i>
+                  <h1>스쿼트</h1>
                 </div>
-              </Col>
-              <Col> </Col>
-            </Row>
-          </Container>
+              </div>
+            </p>
+
+            <p href="" alt="" onClick={() => { alert("준비중입니다") }}>
+              <div className="card-item">
+                <div className="card-inner">
+                  <i className="fas fa-medal">LUNGE</i>
+                  <h1>런지</h1>
+                </div>
+              </div>
+            </p>
+
+            <p href="" alt="" onClick={() => { alert("준비중입니다") }}>
+              <div className="card-item">
+                <div className="card-inner">
+                  <i className="fas fa-medal">PUSH UP</i>
+                  <h1>푸시업</h1>
+                </div>
+              </div>
+            </p>
+
+            <p href="" alt="" onClick={() => {
+                this.props.getUserInfo();
+                // this.props.getTotalCount();
+                this.props.history.push('/Mypage');
+              }}>
+              <div className="card-item">
+                <div className="card-inner">
+                <i className="fas fa-medal">MY PAGE</i>
+                  <h1>마이페이지</h1>
+                </div>
+              </div>
+            </p>
+          </div>
         </Background>
       )
     } else {
--- client/src/Pages/Result.js
+++ client/src/Pages/Result.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
-import { Container, Row, Col, Alert } from 'react-bootstrap';
+import { Container, Row, Col, Alert, Button } from 'react-bootstrap';
 
 
 import styled from 'styled-components';
@@ -28,26 +28,39 @@ const Background = styled.div`
     -moz-osx-font-smoothing: grayscale;  
 `;
 
+
+const Center = styled.div`
+    display: flex; 
+    height: 50%; 
+    justify-content: center; 
+    align-items: center;
+`;
+
+
 function Result(props) {
     return (
         <Background>
-            <Container>
-                <Row>
-                    <Col></Col>
-                    <Col xs={7}>
-                        <Alert variant="success">
-                            <Alert.Heading>Success</Alert.Heading></Alert>
-                        <p />
-                        <Alert variant="secondary">방금 총 {props.count}개의 스쿼트를 수행했습니다!<p />
+            <Center>
+
+                <Container>
+                    <Row>
+                        <Col></Col>
+                        <Col xs={7}>
+                            <Alert variant="success">
+                                <Alert.Heading>Success</Alert.Heading></Alert>
+                            <p />
+                            <Alert variant="secondary">방금 총 {props.count}개의 스쿼트를 수행했습니다!<p />
                     총 {props.count * 0.5}칼로리를 소모하였습니다.<p />
                     자세한 정보는 Mypage에서 확인하실 수 있습니다.</Alert>
-                        <p />
-                        <button onClick={() => { props.history.push('/Mypage') }}>Mypage</button>
-                        <button onClick={() => { props.history.push('/Home') }}>Home</button>
-                    </Col>
-                    <Col></Col>
-                </Row>
-            </Container>
+                            <p />
+                            <Button variant="secondary" onClick={() => { props.history.push('/Mypage') }}>Mypage</Button>
+                        &nbsp;
+                        <Button variant="secondary" onClick={() => { props.history.push('/Home') }}>Home</Button>
+                        </Col>
+                        <Col></Col>
+                    </Row>
+                </Container>
+            </Center>
         </Background>
     )
 }
